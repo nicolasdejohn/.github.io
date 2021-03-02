1f801f00ba2b94e606b178bbed2ead251568257b
nickdejohn <49886199+nickdejohn@users.noreply.github.com>
2021-03-01 19:46:12 -0500

    finished 8-5 in CSC 121

--- csc 121/Chapter 8/NicolasDeJohn_lab8-5.py
+++ csc 121/Chapter 8/NicolasDeJohn_lab8-5.py
@@ -1,48 +1,49 @@
-# Nicolas DeJohn | Chapter 7-2 Lab | January 24th 2021
+# Nicolas DeJohn | Chapter 8-5 Lab | March 1st 2021
+'''The purpose of this program is to format a phone number that contains letters into one that contains only numbers while mainting the '###-###-####' format.'''
+
+#import os 
+#os.chdir(r"C:\Users\Nick\Desktop\CPCC\.github.io\csc 121\Chapter 8")
 
 
-import os 
-os.chdir(r"C:\Users\Nick\Desktop\CPCC\.github.io\csc 121\Chapter 8")
-def main():
-    userPhoneNumber = str(input("Enter the number in the format of XXX-XXX-XXXX: "))
-    userPhoneNumberSplit = userPhoneNumber.split("-")
+def main(): # Defines the main function
 
-    weird_character = False
+    userPhoneNumber = str(input("Enter the number in the format of XXX-XXX-XXXX: ")) # Asks the user for a phone number in the correct format and converts to a string.
+    userPhoneNumberSplit = userPhoneNumber.replace("-", "") # Formats the users input by removing the hyphens.
+    numericPhoneNumber = '' # This variable serves as a blank canvas to store the new converted number.
+    convertedNumber = phoneNumberConversion(userPhoneNumberSplit, numericPhoneNumber) # This variable holds the returned value of the function.
+    print(convertedNumber) # Prints the converted phone number.
 
-    count = 0
+def phoneNumberConversion(userPhoneNumberSplit, numericPhoneNumber): # Defines the function that converts the users input.
+    count = 0 # The next two variables will be used to track when to add the hyphens back in...
+    sectionCount = 0 # ..this is use to track the sections of the number groupings..
+    for ch in userPhoneNumberSplit: # For character in the string..
+        # I won't write a comment on every line but this conditional set tests each letter in ch and adds the properly assigned number to the new string.
+        if ch.isdigit():  
+            numericPhoneNumber += ch
+        elif ch.upper() in 'ABC':
+            numericPhoneNumber += '2'
+        elif ch.upper() in 'DEF':
+            numericPhoneNumber += '3'
+        elif ch.upper() in 'GHI':
+            numericPhoneNumber += '4'
+        elif ch.upper() in 'JKL':
+            numericPhoneNumber += '5'
+        elif ch.upper() in 'MNO':
+            numericPhoneNumber += '6'
+        elif ch.upper() in 'PQRS':
+            numericPhoneNumber += '7'
+        elif ch.upper() in 'TUV':
+            numericPhoneNumber+= '8'
+        elif ch.upper() in 'WXYZ':
+            numericPhoneNumber += '9'
+        count += 1 # Adding to the count until it reaches 3. Then...
+        if count == 3 and sectionCount != 2: # I'm using count to keep track of every 3 numbers and then inserting a hyphen.
+            numericPhoneNumber += '-' # Adds the hyphen to the string.
+            count = 0 # Resets the count for the next number grouping.
+            sectionCount += 1 # Because the last set of numbers are 4 digits, I'm using this to stop after the seconding grouping of numbers.
+    return numericPhoneNumber # Show me the money. 
 
-    numericPhoneNumber = ''
-
-    while weird_character == False and count < 3:
-        for ch in userPhoneNumberSplit:
-            if ch.isdigit():
-                userPhoneNumberSplit += ch
-            elif ch.upper() in 'ABC':
-                userPhoneNumberSplit += '2'
-            elif ch.upper() in 'DEF':
-                userPhoneNumberSplit += '3'
-            elif ch.upper() in 'GHI':
-                userPhoneNumberSplit += '4'
-            elif ch.upper() in 'JKL':
-                userPhoneNumberSplit += '5'
-            elif ch.upper() in 'MNO':
-                userPhoneNumberSplit += '6'
-            elif ch.upper() in 'PQRS':
-                userPhoneNumberSplit += '7'
-            elif ch.upper() in 'TUV':
-                userPhoneNumberSplit += '8'
-            elif ch.upper() in 'WXYZ':
-                userPhoneNumberSplit += '9'
-            else: weird_character = True
-        if count != 2:
-            numericPhoneNumber += "-"
-        count += 1
-    if weird_character:
-        print("weird character")
-    else:
-        print(numericPhoneNumber)
-
-main()
+main() # Ta-daaa
 
 
 
