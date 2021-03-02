f3870f1648a1341e43adcfec6933002f2c99a223
nickdejohn <49886199+nickdejohn@users.noreply.github.com>
2021-03-02 18:14:23 -0500

    added personal stuff

--- csc 121/Chapter 8/NicolasDeJohn_lab8-2_2.py
+++ csc 121/Chapter 8/NicolasDeJohn_lab8-2_2.py
@@ -1,16 +1,17 @@
 # Nicolas DeJohn | Chapter 8-2 Lab | March 1st 2021
 """ 
-The purpose of this program is to prompt the user for a series of numbers with no spaces or special characters, and totaling up those numbers together.
+The purpose of this program is to open stringONumbers.txt for reading, get the numbers listed as a string, and use a loop 
+to get the sum of all the numbers in the string.
 """
 
-"""Using this as fail-safe to get my python programs to work.."""
+"""Still using this fail-safe to get my python programs to work.."""
 #import os 
 #os.chdir(r"C:\Users\Nick\Desktop\CPCC\.github.io\csc 121\Chapter 8")
 
 stringOfNumbers = str(input("Enter a series of numbers with no spaces or special characters: ")) # Gets the users string of numbers.
 total = 0 # initiates the total variable.
 
-for ch in stringOfNumbers.replace(" ", ""): # Iterate over each index of the string. I used the replace method in case the space key was pressed during input.
+for ch in stringOfNumbers: # iterate over each index of the string
     total += int(ch) # adds the current index of the string (converted to an integer) to the total variable.
 
 
